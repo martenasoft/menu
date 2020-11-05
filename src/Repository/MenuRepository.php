557ae8a5cb60c88ff96144f53a52700baaa799b6
primawebsystems <kostiaGm@gmail.com>
2020-11-05 12:24:34 +0200

    Removed extend abstract NestedSetsRepository and moved Nested sets classes in constructor arguments

--- src/Repository/MenuRepository.php
+++ src/Repository/MenuRepository.php
@@ -2,18 +2,30 @@
 
 namespace MartenaSoft\Menu\Repository;
 
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\ORM\QueryBuilder;
 use Doctrine\Persistence\ManagerRegistry;
-use MartenaSoft\Common\Repository\AbstractNestedSetServiceRepository;
+use MartenaSoft\Common\Entity\NestedSetEntityInterface;
 use MartenaSoft\Menu\Entity\Menu;
+use MartenaSoft\NestedSets\Entity\NodeInterface;
+use MartenaSoft\NestedSets\Repository\NestedSetsMoveItemsInterface;
+use MartenaSoft\NestedSets\Repository\NestedSetsMoveUpDown;
+use MartenaSoft\NestedSets\Repository\NestedSetsMoveUpDownInterface;
 
-class MenuRepository extends AbstractNestedSetServiceRepository
+class MenuRepository extends ServiceEntityRepository
+    implements NestedSetsMoveItemsInterface, NestedSetsMoveUpDownInterface
 {
     protected string $alias = 'm';
+    private NestedSetsMoveNode $nestedSetsMoveNode;
 
-    public function __construct(ManagerRegistry $registry)
-    {
+    public function __construct(
+        ManagerRegistry $registry,
+        NestedSetsMoveItemsInterface $nestedSetsMoveNode,
+        NestedSetsMoveUpDownInterface $moveUpDown
+    ) {
         parent::__construct($registry, Menu::class);
+        $this->nestedSetsMoveNode = $nestedSetsMoveNode;
+        $this->moveUpDown = $moveUpDown;
     }
 
     public function getAllQueryBuilder(): QueryBuilder
@@ -22,5 +34,15 @@ class MenuRepository extends AbstractNestedSetServiceRepository
             ->createQueryBuilder($this->alias)
             ->orderBy("{$this->alias}.tree", "ASC")
             ->addOrderBy("{$this->alias}.lft", "ASC");
+    }
+
+    public function move(NestedSetEntityInterface $node, ?NestedSetEntityInterface $parent): void
+    {
+        $this->nestedSetsMoveNode->move($node, $parent);
+    }
+
+    public function change(NodeInterface $node, bool $isUp = true): void
+    {
+        $this->moveUpDown->change($node, $isUp);
     }
 }
